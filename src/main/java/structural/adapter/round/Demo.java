db3ab62c8f23f493ca80f0ec1d602a75d47091c7
EricTan2002 <tanyouming1@gmail.com>
2022-11-20 12:30:51 +1100

    ~

---
+++ src/main/java/structural/adapter/round/Demo.java
@@ -0,0 +1,24 @@
+package structural.adapter.round;
+
+public class Demo {
+    public static void main(String[] args) {
+        RoundHole hole = new RoundHole(5);
+        RoundPeg rpeg = new RoundPeg(5);
+        if (hole.fits(rpeg)) {
+            System.out.println("Round peg r5 fits round hole r5");
+        }
+
+        SquarePeg smallSqPeg = new SquarePeg(2);
+        SquarePeg largeSqPeg = new SquarePeg(20);
+
+        // Adapter solves the problem.
+        SquarePegAdapter smallSqPegAdapter = new SquarePegAdapter(smallSqPeg);
+        SquarePegAdapter largeSqPegAdapter = new SquarePegAdapter(largeSqPeg);
+        if (hole.fits(smallSqPegAdapter)) {
+            System.out.println("Square peg w2 fits round hole r5.");
+        }
+        if (!hole.fits(largeSqPegAdapter)) {
+            System.out.println("Square peg w20 does not fit into round hole r5.");
+        }
+    }
+}
